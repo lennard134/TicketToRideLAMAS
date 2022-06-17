665fe388bc08f1718ba76fa7387dac54d84ce3a0
Jeroen <jeroen.h.vangelder@gmail.com>
2022-06-17 17:26:16 +0200

    Fixed scaling

--- src/pygame_test.py
+++ src/pygame_test.py
@@ -12,6 +12,8 @@ from src.model.map.City import City
 # Settings
 SCREEN_WIDTH = 1200
 SCREEN_HEIGHT = 1000
+WIDTH_BUFFER = 2
+HEIGHT_BUFFER = 2
 RADIUS = 12
 
 
@@ -22,18 +24,14 @@ class GameBoard(object):
     def __init__(self, ttr: TicketToRide):
         self.ttr = ttr
 
-    def highest_xy(self, cities: list[City], min_x: float, min_y: float):
+    def highest_xy(self, cities: list[City]):
         """
         Determines maximum x and y coordinates of cities to allow for accurate scaling to window size
         """
         max_y, max_x = cities[0].coordinates
-        max_x -= min_x
-        max_y -= min_y
 
         for city in cities:
             height, width = city.coordinates
-            height -= min_y
-            width -= min_x
             max_x = width if width > max_x else max_x
             max_y = height if height > max_y else max_y
 
@@ -78,12 +76,18 @@ class GameBoard(object):
         screen.fill((255, 255, 255))
         contents.fill((255, 255, 255))
 
-        # screen.fill((0, 0, 0,))
-
         font1 = pygame.font.SysFont('chalkduster.ttf', 24)
 
         min_x, min_y = self.lowest_xy(list(self.ttr.board.cities.values()))
-        max_x, max_y = self.highest_xy(list(self.ttr.board.cities.values()), min_x, min_y)
+        max_x, max_y = self.highest_xy(list(self.ttr.board.cities.values()))
+
+        min_x -= WIDTH_BUFFER
+        max_x += WIDTH_BUFFER
+        min_y -= HEIGHT_BUFFER
+        max_y += HEIGHT_BUFFER
+
+        max_x -= min_x
+        max_y -= min_y
 
         x_scaling = SCREEN_WIDTH / max_x
         y_scaling = SCREEN_HEIGHT / max_y
@@ -109,11 +113,11 @@ class GameBoard(object):
 
             x = width * x_scaling
             y = SCREEN_HEIGHT - height * y_scaling
-            # draw cities as circles
 
             lt_lim, rt_lim, tp_lim, bt_lim = self.update_limits(x - RADIUS, y - RADIUS, lt_lim, rt_lim, tp_lim, bt_lim)
             lt_lim, rt_lim, tp_lim, bt_lim = self.update_limits(x + RADIUS, y + RADIUS, lt_lim, rt_lim, tp_lim, bt_lim)
 
+            # draw cities as circles
             pygame.draw.circle(contents, (255, 0, 0), (x, y), radius=RADIUS)
 
             # draw text on center of cities
@@ -129,12 +133,7 @@ class GameBoard(object):
 
             contents.blit(text, (txt_left, txt_top))
 
-        content_scale_x = SCREEN_WIDTH / (rt_lim - lt_lim)
-        content_scale_y = SCREEN_HEIGHT / (bt_lim - tp_lim)
-
-
-
-        screen.blit(contents, (0, 0))
+        screen.blit(contents, (WIDTH_BUFFER / 2, HEIGHT_BUFFER / 2))
 
         pygame.display.update()
 
